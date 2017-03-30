0fee6a066b2884d2b5810dc0124d6e00549cab8e
Yarden Cohen <yardenc90@gmail.com>
2017-03-30 10:58:52 +0300

    more changes

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'jbuilder', '~> 2.5'
 
 gem "react_on_rails", "~> 6"
 
+gem 'bootstrap', '~> 4.0.0.alpha6'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/models/resturant.rb
+++ app/models/resturant.rb
@@ -1,4 +1,10 @@
 class Resturant < ApplicationRecord	
+	validates :name, presence: true
+	validates :genre, presence: true
+	validates :rating, presence: true
+	validates :address, presence: true
+	validates :max_delivery_time, presence: true
+
 	def get_cuisine_icon
 		cuisine = Cuisine.find_by(title: self.genre)
 		if cuisine
@@ -6,7 +12,7 @@ class Resturant < ApplicationRecord
 		else
 		  image = Cuisine.find_by(title: 'Other').image
 		end
-		
+
 		image
 	end
 end
